267b8351c06e4002f737f83991d4d172782276e9
leo0111001 <leo.biljesko@edu.fh-joanneum.at>
2026-01-25 12:34:17 +0100

    modified:   src/tt_um_ECM24_serv_soc_top.sv

--- src/tt_um_ECM24_serv_soc_top.sv
+++ src/tt_um_ECM24_serv_soc_top.sv
@@ -16,7 +16,7 @@ module tt_um_ECM24_serv_soc_top
  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
  input  wire       clk,      // clock
- input  wire       rst_n     // reset_n - low to reset
+ input  wire       rst_n,     // reset_n - low to reset
  input wire  wb_clk,    // Wishbone clock
  input wire  wb_rst,    // Wishbone reset
  output wire q,          // GPIO output
