9c726f1fd6c4c4df6c165b2da53371ae62832473
leo0111001 <leo.biljesko@edu.fh-joanneum.at>
2026-01-25 11:52:13 +0100

    new file:   src/ECM24_serv_soc_top.sv 	new file:   src/RAM32.v 	deleted:    src/project.v 	new file:   src/serv/serv_aligner.v 	new file:   src/serv/serv_alu.v 	new file:   src/serv/serv_bufreg.v 	new file:   src/serv/serv_bufreg2.v 	new file:   src/serv/serv_compdec.v 	new file:   src/serv/serv_csr.v 	new file:   src/serv/serv_ctrl.v 	new file:   src/serv/serv_debug.v 	new file:   src/serv/serv_decode.v 	new file:   src/serv/serv_immdec.v 	new file:   src/serv/serv_mem_if.v 	new file:   src/serv/serv_rf_if.v 	new file:   src/serv/serv_rf_ram_if.v 	new file:   src/serv/serv_state.v 	new file:   src/serv/serv_top.v 	new file:   src/serv/servile.v 	new file:   src/serv/servile_arbiter.v 	new file:   src/serv/servile_mux.v 	new file:   src/serv_ram32_if.sv 	new file:   src/spi_sram.sv 	new file:   src/subservient_gpio.v

---
+++ src/RAM32.v
@@ -0,0 +1,35 @@
+/// sta-blackbox
+
+`default_nettype none
+
+module RAM32(
+`ifdef USE_POWER_PINS
+  input wire VPWR,
+  input wire VGND,
+`endif
+  input wire CLK,
+  input wire [3:0] WE0,
+  input wire EN0,
+  input wire [4:0] A0,
+  input wire [31:0] Di0,
+  output reg [31:0] Do0
+);
+    reg [31:0] RAM[31:0];
+    integer i;
+    initial begin
+        for (i = 0; i < 32; i = i + 1) begin
+            RAM[i] = 32'h00000000;
+        end
+    end
+    always @(posedge CLK)
+        if(EN0) begin
+            Do0 <= RAM[A0];
+            if(WE0[0]) RAM[A0][ 7: 0] <= Di0[7:0];
+            if(WE0[1]) RAM[A0][15:8] <= Di0[15:8];
+            if(WE0[2]) RAM[A0][23:16] <= Di0[23:16];
+            if(WE0[3]) RAM[A0][31:24] <= Di0[31:24];
+        end
+        else
+            Do0 <= 32'b0;
+
+endmodule
